f3b6cd1459676d690c4071d89911e8f567f2c5ae
Leandro Dourado <leandrotux@hotmail.com>
2022-02-12 11:23:02 -0300

    ïnitial project

---
+++ lib/Home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Minhas anotações"),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightGreen,
+        foregroundColor: Colors.white,
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:notas_diarias/Home.dart';
+
+void main() => runApp(MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    ));
